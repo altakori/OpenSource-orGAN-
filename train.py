6c604972043be8e79edf1a971e65f663543035bb
be0k <befinetuning@gmail.com>
2023-12-05 11:16:22 +0900

    upload fixed files and env.yaml

--- train.py
+++ train.py
@@ -8,14 +8,15 @@ from torch import optim
 import argparse
 #custom library
 import model
-import data
+import utils
+
 def train():
     wandb.login()
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--location', type=str, help="data location", default='./data')
     parser.add_argument('--lr', type=float, help="learning rate", default=0.001)
-    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--epoch', type=int, default=100)
     parser.add_argument('--num_workers', type=int, default=8)
     parser.add_argument('--beta1', type=float, default=0.5)
@@ -39,12 +40,13 @@ def train():
 
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-    data_transform = T.Compose([T.ToTensor(),
-                                T.Normalize([0.5, 0.5, 0.5]),
-                                T.Resize((256,256)),
+    data_transform = T.Compose([T.Resize((256,256)),
+                                T.ToImage(),
+                                T.ToDtype(torch.float32, scale=True),
+                                T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
                                 ])
 
-    data = data.CustomDataset(config.location, transform = data_transform)
+    data = utils.CustomDataset(config.location, transform = data_transform)
 
     data_loader = DataLoader(data,
                             batch_size=config.batch_size,
@@ -54,36 +56,33 @@ def train():
 
     model_gen = model.GeneratorUNet()
     model_dis = model.Discriminator()
-    model_gen.apply(model.initialize_weights);
-    model_dis.apply(model.initialize_weights);
-    model_gen.to(device)
-    model_dis.to(device)
+    model_gen.apply(model.initialize_weights)
+    model_dis.apply(model.initialize_weights)
 
     criterion_gen = nn.BCELoss()
     criterion_dis = nn.L1Loss()
 
     lambda_pixel = 100
 
-    patch = (1, 256//2**3, 256//2**4)
-    optimizer_gen = optim.AdamW(model_gen.parameters(), lr=config.lr, betas=config.beta)
-    optimizer_dis = optim.AdamW(model_dis.parameters(), lr=config.lr, betas=config.beta)
+    patch = (1, 16, 16)
+    optimizer_gen = optim.Adam(model_gen.parameters(), lr=config.lr, betas=config.beta)
+    optimizer_dis = optim.Adam(model_dis.parameters(), lr=config.lr, betas=config.beta)
 
     model_gen.train()
     model_dis.train()
+    model_gen, model_dis = model_gen.to(device), model_dis.to(device)
 
     for epoch in range(config.epoch):
         for face, comic in data_loader:
             size = comic.size(0)
-            face.to(device)
-            comic.to(device)
+            face, comic = face.to(device), comic.to(device)
             face_label = torch.ones(size, *patch, requires_grad=False).to(device)
             comic_label = torch.zeros(size, *patch, requires_grad=False).to(device)
 
             model_gen.zero_grad()
             
-            fake_comic = model_gen(face)
-
-            out_dis = model_dis(fake_comic, face)
+            fake_comic = model_gen(face).to(device)
+            out_dis = model_dis(fake_comic, face).to(device)
 
             gen_loss = criterion_gen(out_dis, face_label)
             pixel_loss = criterion_dis(fake_comic, comic)
@@ -93,10 +92,10 @@ def train():
 
             model_dis.zero_grad()
             
-            out_dis = model_dis(comic, face)
+            out_dis = model_dis(comic, face).to(device)
             real_loss = criterion_gen(out_dis, face_label)
 
-            out_dis = model_dis(fake_comic.detach(), face)
+            out_dis = model_dis(fake_comic.detach(), face).to(device)
             fake_loss = criterion_gen(out_dis, comic_label)
 
             d_loss = (real_loss + fake_loss) / 2.
@@ -106,7 +105,7 @@ def train():
 
         torch.save({'gen':model_gen.state_dict(),
                     'dis':model_dis.state_dict()},
-                    f'./model/{epoch}')
+                    f'./model/{epoch}.pt')
         
     wandb.finish()
 
--- utils.py
+++ utils.py
@@ -1,20 +1,20 @@
 from torch.utils.data import Dataset
 from os.path import join
 from os import listdir
+from PIL import Image
 import cv2
 
 class CustomDataset(Dataset):
     def __init__(self, path2img, transform=None):
         super().__init__()
-        self.path2real = join(path2img, 'real')
-        self.path2comic = join(path2img, 'comic')
+        self.path2real = join(path2img, 'faces')
+        self.path2comic = join(path2img, 'comics')
         self.img_filenames = [x for x in listdir(self.path2comic)]
         self.transform = transform
 
     def __getitem__(self, index):
-        real = cv2.imread(join(self.path2real, self.img_filenames[index]))
-        comic = cv2.imread(join(self.path2comic, self.img_filenames[index]))
-        
+        real = Image.open(join(self.path2real, self.img_filenames[index])).convert('RGB')
+        comic = Image.open(join(self.path2comic, self.img_filenames[index])).convert('RGB')
         if self.transform:
             real = self.transform(real)
             comic = self.transform(comic)
