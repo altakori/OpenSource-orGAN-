fed32f5618ad0e68eadc9c200e7de8551cf58d92
be0k <befinetuning@gmail.com>
2023-12-05 13:08:23 +0900

    fix wandb log

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ from torch.utils.data import Dataset
 from os.path import join
 from os import listdir
 from PIL import Image
+import torchvision.transforms.v2 as T
+import torch
 import cv2
 
 class CustomDataset(Dataset):
@@ -22,3 +24,12 @@ class CustomDataset(Dataset):
 
     def __len__(self):
         return len(self.img_filenames)
+    
+
+def unnormalize(image):
+    image = (image*0.5 + 0.5) * 255
+    a = T.compose([T.ToDtype(torch.int32, scale=True),
+                    T.ToPILImage()
+                    ])
+    a(image)
+    return a
